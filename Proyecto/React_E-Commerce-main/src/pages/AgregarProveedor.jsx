40eea5ff814915af83f20140b83396e153884d83
Rodri541 <rodridiazmosegue4@gmail.com>
2024-12-22 00:25:01 -0300

    Prueba18

--- Proyecto/React_E-Commerce-main/src/pages/AgregarProveedor.jsx
+++ Proyecto/React_E-Commerce-main/src/pages/AgregarProveedor.jsx
@@ -1,5 +1,5 @@
 import API_URL from "../config";
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef } from "react";
 import { Sidebar, Navbar } from "../components";
 import toast from "react-hot-toast";
 
