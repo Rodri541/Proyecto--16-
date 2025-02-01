f68b5cebaf6837bfbf17fa9582afdba7382bbea7
Rodri541 <68251000+Rodri541@users.noreply.github.com>
2025-02-01 01:45:31 -0300

    Delete Proyecto directory

--- Proyecto/ApiProject/webhooks.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { handleWebhook } = require('./webhook.controller.js');
-
-const router = Router();
-
-router.post("/webhook", handleWebhook);
-
-module.exports = router;
--- Proyecto/React_E-Commerce-main/src/config.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "https://apiprueba-ekhkb4ebchfqhubg.canadacentral-01.azurewebsites.net/"; // Cambia el puerto si tu API usa otro
-
-//"http://localhost:3000"
-
-//https://apiprueba-ekhkb4ebchfqhubg.canadacentral-01.azurewebsites.net/
-
-export default API_URL;
